42c0c8c54c51ef544ce145b9f629a817e74d601e
hehang <2927705354@qq.com>
2021-01-20 13:52:39 +0800

    project2

--- src/main.js
+++ src/main.js
@@ -10,9 +10,7 @@ Vue.use(element)
 // 引入vant-ui  vant
 import Vant from 'vant';
 import 'vant/lib/index.css';
-
-Vue.use(Vant);
-
+Vue.use(Vant); 
 // 引入接口
 import axios from '@/until/api'
 Vue.prototype.$axios=axios
@@ -21,9 +19,10 @@ Vue.prototype.$axios=axios
 import echarts from 'echarts'
 Vue.prototype.$echarts=echarts
 
-// 移动端适配  cnpm install lib-flexible postcss-pxtorem --save-dev
+// 移动端rem适配  cnpm install lib-flexible postcss-pxtorem --save-dev
 import 'lib-flexible/flexible';
 
+// vuex数据持久化 cnpm i vuex-persist -D
 Vue.config.productionTip = false
 
 new Vue({
